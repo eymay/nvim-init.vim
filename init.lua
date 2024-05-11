e8a72c37343aab2f04e93eb57ba25c8bab2d74e7
Eymen Ünay <eymenunay@outlook.com>
2024-05-11 23:01:45 +0300

    Minimal usable config with lazy.nvim

--- init.lua
+++ init.lua
@@ -1,14 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+        lazypath
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("lazy").setup("plugins")
+vim.api.nvim_set_option("clipboard", "unnamed")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+require('telescope').load_extension('fzf')
+
+vim.api.nvim_set_keymap('n', '<leader>b', ':NvimTreeFindFileToggle<CR>',
+                        {noremap = true, silent = true})
+
