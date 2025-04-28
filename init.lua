12d6ab515e10d0ce63d717c7c37671a525e075be
Eymen Ünay <eymenunay@outlook.com>
2025-04-28 11:27:05 +0100

    Hot fix for neovim 0.11 update

--- init.lua
+++ init.lua
@@ -14,6 +14,18 @@ vim.g.maplocalleader = " "
 require("lazy").setup("plugins")
 vim.api.nvim_set_option("clipboard", "unnamed")
 
+-- Configure diagnostics for Neovim 0.11
+-- Virtual text is disabled by default in 0.11
+vim.diagnostic.config({
+    virtual_text = true,  -- Re-enable if you want virtual text diagnostics
+    severity_sort = true,
+    underline = true,
+    update_in_insert = false,
+    float = {
+        source = 'always',
+    },
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -24,3 +36,13 @@ require('telescope').load_extension('fzf')
 vim.api.nvim_set_keymap('n', '<leader>b', ':NvimTreeFindFileToggle<CR>',
                         {noremap = true, silent = true})
 
+vim.g.markdown_fenced_languages = {'mlir'}
+
+-- New default LSP keymaps added in Neovim 0.11
+-- Uncomment if you want to use them
+-- vim.keymap.set('n', 'grn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
+-- vim.keymap.set('n', 'grr', vim.lsp.buf.references, { desc = 'Show references' })
+-- vim.keymap.set('n', 'gri', vim.lsp.buf.implementation, { desc = 'Show implementation' })
+-- vim.keymap.set('n', 'gO', vim.lsp.buf.document_symbol, { desc = 'Document symbols' })
+-- vim.keymap.set({'n', 'v'}, 'gra', vim.lsp.buf.code_action, { desc = 'Code action' })
+-- vim.keymap.set({'i', 's'}, '<C-s>', vim.lsp.buf.signature_help, { desc = 'Signature help' })
